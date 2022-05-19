33056e2c6846905fa6c9380b5afd993f6175e47b
Aleksandr Lapshin <Lapshin_A_A@bk.ru>
2022-05-19 03:03:20 +0300

    Profile.storyboard set as main

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -8,13 +8,9 @@
 import UIKit
 
 class ProfileView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateOfBirthLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileTextView: UITextView!
 }
